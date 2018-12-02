9c08598785895345b57e19a0ef7ce21144bdf279
Arthur Bates <aabatesj@gmail.com>
2018-12-02 15:10:01 -0500

    Updates
    Hash tables - ex1 & ex2
    Theory - booleans 1, 2 ,& 3
    
    WIP:
    state machine drawing

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -7,6 +7,23 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
   // YOUR CODE HERE
+  for (int i = 0; i < length; i++)
+  {
+    int complement = hash_table_retrieve(ht, limit - weights[i]);
+    if (complement != -1)
+    {
+      //Found a match
+      Answer *answer = malloc(sizeof(Answer));
+      answer->index_1 = i;
+      answer->index_2 = complement;
+      destroy_hash_table(ht);
+      return answer;
+    }
+    else
+    {
+      hash_table_insert(ht, weights[i], i);
+    }
+  }
 
   return NULL;
 }
--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -10,13 +10,35 @@ char **reconstruct_trip(Ticket **tickets, int length)
   char **route = malloc(length * sizeof(char *));
 
   // YOUR CODE HERE
+  for (int i = 0; i < length; i++)
+  {
+    if (strcmp(tickets[i]->source, "NONE") == 0)
+    {
+      route[0] = tickets[i]->destination;
+    }
+    hash_table_insert(hash, tickets[i]->source, tickets[i]->destination);
+  }
+
+  for (int j = 1; j < length; j++)
+  {
+    if (hash_table_retrieve(hash, route[j - 1]) != NULL)
+    {
+      route[j] = hash_table_retrieve(hash, route[j - 1]);
+    }
+    else
+    {
+      route[j] = "NONE";
+    }
+  }
+  destroy_hash_table(hash);
 
   return route;
 }
 
 void print_route(char **route, int length)
 {
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     printf("%s\n", route[i]);
   }
 }
